5f9b1b3f67afb532c869066410059e902ced0cfc
jujubea22 <114043890+jujubea22@users.noreply.github.com>
2023-03-12 21:15:07 -0400

    Update product-routes.js

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -24,8 +24,7 @@ router.post('/', (req, res) => {
       stock: 3,
       tagIds: [1, 2, 3, 4]
     }
-  */
-  Product.create(req.body)
+  */Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
