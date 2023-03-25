346610d9e00089928be947efcc2d5011e7329057
jujubea22 <114043890+jujubea22@users.noreply.github.com>
2023-03-24 21:48:43 -0400

    Update index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,20 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+product.belongsTo(Category);
 
 // Categories have many Products
+category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
-
+products.belongToMany (Tags, {
+  through:'productTag',
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
+tag.belongsToMany(product, { through: 'productTag',
+                            foreignKey: 'tag_id' });
+
 
 module.exports = {
   Product,
