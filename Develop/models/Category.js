cbfb8ac0d5ea42858827f7582361346096b9746e
jujubea22 <114043890+jujubea22@users.noreply.github.com>
2023-03-24 21:41:02 -0400

    Update Category.js

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,7 +7,14 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
-  },
+    {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+ 
   {
     sequelize,
     timestamps: false,
